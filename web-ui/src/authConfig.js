c058bab31a07eff8c51b4522e7b076de7c6807ab
Mert Ozdag <mert.ozdag@gmail.com>
2026-09-15 20:58:54 -0400

    auth configurations moved to env

--- web-ui/src/authConfig.js
+++ web-ui/src/authConfig.js
@@ -4,9 +4,10 @@ const BASE = `${(import.meta.env.VITE_REDIRECT_URL ?? '').replace(/\/$/, '')}/`
 
 export const msalConfig = {
   auth: {
-    clientId: '85124712-2ee2-4e87-b302-d82fb46aceda',
-    authority: 'https://login.microsoftonline.com/fa60987f-99d7-464a-9994-9229f4e8c6e2',
+    clientId: import.meta.env.VITE_MSAL_CLIENT_ID,
+    authority: import.meta.env.VITE_MSAL_AUTHORITY,
     redirectUri: BASE,
+    knownAuthorities: ["localhost:5100"], // Required for local/custom identity providers
   },
   cache: {
     cacheLocation: 'sessionStorage',
@@ -19,7 +20,7 @@ export const loginRequest = {
 }
 
 export const apiTokenRequest = {
-  scopes: ['api://aiindex-api/access_as_user'],
+  scopes: [import.meta.env.VITE_MSAL_API_SCOPE],
 }
 
 export const msalInstance = new PublicClientApplication(msalConfig)
